34998fe6a06668e901f1b369301f8d15f69c0ff6
Devdutt Shenoi <devdutt@outlook.in>
2022-05-10 11:02:35 +0530

    Add check for docker.sock

--- release.sh
+++ release.sh
@@ -2,6 +2,13 @@
 TARGETS=$@
 BUILD_DIR="build/"
 
+if test -e /var/run/docker.sock; then
+    echo "Docker daemon volume is attached!"
+else
+    echo "Volume is not attached! Please attach it first, then re-run this script!"
+    exit 1
+fi
+
 for TARGET in ${TARGETS}
 do
     echo "Building uplink for ${TARGET}"
