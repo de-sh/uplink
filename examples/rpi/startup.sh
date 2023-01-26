3e8e05f188725f2e7bf07547af8621d487ea9b9c
saikiran-1997 <ysaikiran1997@gmail.com>
2023-01-26 13:43:01 +0530

    fix: Added startup.sh

---
+++ examples/rpi/startup.sh
@@ -0,0 +1,53 @@
+root_part=`awk -F"root=" '{ print $NF; }' /proc/cmdline | cut -d" " -f1`
+
+TWO_OK=/boot/two_ok
+TWO_BOOT=/boot/two
+TWO_FAILED=/boot/two_failed
+TWO_DOWNLOAD=/mnt/download/two
+THREE_OK=/boot/three_ok
+THREE_BOOT=/boot/three
+THREE_FAILED=/boot/three_failed
+THREE_DOWNLOAD=/mnt/download/three
+
+if [ -f "$THREE_DOWNLOAD" ]
+then
+	if [ ${root_part: -1} = "2" ]
+	then
+		# Boot failed
+		rm -rf $THREE_BOOT
+		rm -rf $THREE_FAILED 
+		touch $TWO_BOOT
+		touch $TWO_DOWNLOAD
+		if [ -e $THREE_DOWNLOAD ]
+		then
+			rm -rf $THREE_DOWNLOAD
+		fi
+		# Notify uplink
+	elif [ ${root_part: -1} = "3" ]
+	then
+		# Boot successful
+		rm -rf $THREE_FAILED
+		touch $THREE_OK
+	fi
+elif [ -f "$TWO_DOWNLOAD" ]
+then
+	if [ ${root_part: -1} = "3" ]
+	then
+		# Boot failed
+		rm -rf $TWO_BOOT
+		rm -rf $TWO_FAILED
+		touch $THREE_BOOT
+		touch $THREE_DOWNLOAD
+		if [ -f $TWO_DOWNLOAD ]
+		then
+			rm -rf $TWO_DOWNLOAD
+		fi
+		# Notify uplink
+	elif [ ${root_part: -1} = "2" ]
+	then
+		# Boot successful
+		rm -rf $TWO_FAILED
+		touch $TWO_OK
+	fi
+fi
+
