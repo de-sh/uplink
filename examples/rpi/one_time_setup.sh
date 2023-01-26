d4fa5046a8f6e3e6ac31a7d20be3a6ebab2ad16b
saikiran-1997 <ysaikiran1997@gmail.com>
2023-01-26 12:04:27 +0530

    fix: Github URL changed

--- examples/rpi/one_time_setup.sh
+++ examples/rpi/one_time_setup.sh
@@ -2,7 +2,7 @@
 # curl  --proto '=https' --tlsv1.2 -sSf one_time_setup.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/one_time_setup.sh | bash 
 
 # get update_fstab.sh
-curl --proto '=https' --tlsv1.2 -sSf -o update_fstab.sh https://raw.githubusercontent.com/bytebeamio/uplink/main/examples/rpi/update_fstab.sh
+curl --proto '=https' --tlsv1.2 -sSf -o update_fstab.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/update_fstab.sh
 chmod +x ./update_fstab.sh
 ./update_fstab.sh
 mount -a
@@ -12,14 +12,14 @@ cp update_fstab.sh /mnt/download/
 curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/update_fstab_next_root.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/update_fstab_next_root.sh
 
 # get uplink binary
-curl --proto '=https' --tlsv1.2 -sSfL -o /mnt/download/uplink https://github.com/bytebeamio/uplink/releases/download/v1.6.1/uplink-aarch64-unknown-linux-gnu
+curl --proto '=https' --tlsv1.2 -sSfL -o /mnt/download/uplink https://github.com/sai-kiran-y/uplink/releases/download/v1.6.1/uplink-aarch64-unknown-linux-gnu
 
 # get bridge_app
-curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/bridge.py https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/bridge.py
+curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/bridge.py https://raw.githubusercontent.com//sai-kiran-y/uplink/main/examples/rpi/bridge.py
 
 # get systemd script
 mkdir -pv /mnt/download/systemd
-curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/systemd/systemd.sh https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/systemd/systemd.sh
+curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/systemd/systemd.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/systemd/systemd.sh
 
 # get uplink.service
 curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/systemd/uplink.service https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/systemd/uplink.service
@@ -28,13 +28,13 @@ curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/systemd/uplink.service htt
 curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/systemd/bridge.service https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/systemd/bridge.service
 
 # get config.toml 
-curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/config.toml https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/config.toml
+curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/config.toml https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/config.toml
 
 # get reboot.sh
-curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/config.toml https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/reboot.sh
+curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/config.toml https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/reboot.sh
 
 # get find_root.sh
-curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/config.toml https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/find_root.sh
+curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/config.toml https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/find_root.sh
 
 # Make uplink executable
 chmod +x /mnt/download/uplink
@@ -44,7 +44,7 @@ cp /mnt/download/systemd/bridge.service /etc/systemd/system/
 systemctl daemon-reload
 
 # get run_uplink.sh script
-curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/run_uplink.sh https://raw.githubusercontent.com/bytebeamio/sai-kiran-y/main/examples/rpi/run_uplink.sh
+curl --proto '=https' --tlsv1.2 -sSf -o /mnt/download/run_uplink.sh https://raw.githubusercontent.com/sai-kiran-y/uplink/main/examples/rpi/run_uplink.sh
 
 chmod +x /mnt/download/run_uplink.sh
 
